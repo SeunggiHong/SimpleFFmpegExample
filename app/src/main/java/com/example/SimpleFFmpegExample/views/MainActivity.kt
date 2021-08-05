76c90afaf24af4aba98c691cee9e30f12e9ac165
seunggiHong <tmdrl1026@naver.com>
2021-08-06 02:57:57 +0900

    simple ffmpeg app init

---
+++ app/src/main/java/com/example/SimpleFFmpegExample/views/MainActivity.kt
@@ -0,0 +1,85 @@
+package com.example.SimpleFFmpegExample.views
+
+import android.content.pm.PackageManager
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.example.SimpleFFmpegExample.R
+import com.example.SimpleFFmpegExample.utils.Constants
+import com.example.SimpleFFmpegExample.utils.Constants.TAG
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        setupPermissions()
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        when (requestCode) {
+            Constants.REQUEST_CODE -> {
+                if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // Permission is granted. Continue the action or workflow
+                    // in your app.
+                } else {
+                    showPermissionDialog()
+                }
+                return
+            }
+
+        }
+    }
+
+    fun showPermissionDialog() {
+        val dlgPermission = PermissionDialog()
+        dlgPermission.show(supportFragmentManager, "permission")
+    }
+
+    private fun setupPermissions() {
+        val writePermission = ContextCompat.checkSelfPermission(this, android.Manifest.permission.WRITE_EXTERNAL_STORAGE)
+        val readPermission = ContextCompat.checkSelfPermission(this, android.Manifest.permission.READ_EXTERNAL_STORAGE)
+
+        if (writePermission!= PackageManager.PERMISSION_GRANTED && readPermission != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                android.Manifest.permission.READ_EXTERNAL_STORAGE), Constants.REQUEST_CODE)
+        }
+
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d(TAG, "MainActivity - onStart() called")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d(TAG, "MainActivity - onResume() called")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.d(TAG, "MainActivity - onRestart() called")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d(TAG, "MainActivity - onPause() called")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d(TAG, "MainActivity - onStop() called")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG, "MainActivity - onDestroy() called")
+    }
+
+}

8e9251616287b9ca6e35e72cd17265ebb16d9161
seunggiHong <tmdrl1026@naver.com>
2021-08-07 22:41:49 +0900

    videoeditor, pieprogress libraray 추가

---
+++ app/src/main/java/com/example/SimpleFFmpegExample/views/CropActivity.kt
@@ -0,0 +1,11 @@
+package com.example.SimpleFFmpegExample.views
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CropActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_crop2)
+    }
+}
---
+++ app/src/main/java/com/example/SimpleFFmpegExample/views/TrimActivity.kt
@@ -0,0 +1,11 @@
+package com.example.SimpleFFmpegExample.views
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class TrimActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_trim)
+    }
+}
